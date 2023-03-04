9eb83c731b07d9f1b977513092d43823bec8150d
RicardoQuijanoJ <125588354+RicardoQuijanoJ@users.noreply.github.com>
2023-03-04 12:01:38 -0500

    Herencia, polimorfismo, agregación, composición

---
+++ herencia/introHerencia.py
@@ -0,0 +1,7 @@
+def suma (a,b): #   Se crea un metodo (suma) que recibe 2 parametros (a,b)
+    return a+b  #   retorna la suma
+
+def suma(a,b,c):    #   Se crea un metodo (suma) que recibe 3 parametros (a,b,c)
+    return a+b+c    #   retorna la suma de los parametros
+
+print(suma(1,2,3))  #   se imprime el llamado al metodo (suma) y se le pasan los 3 datos
